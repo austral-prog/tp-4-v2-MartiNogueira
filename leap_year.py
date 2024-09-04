b2730b42620fceab0932e8594091394048424e6b
Martiniano <140777358+MartiNogueira@users.noreply.github.com>
2024-09-03 23:27:57 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,25 +1,9 @@
 def leap_year():
-    a = float(input("Ingrese el coeficiente A :"))
-    b = float(input("Ingrese el coeficiente B :"))
+    year = int(input("Ingrese un año: "))
 
-    x1 = float(input("Ingrese el coeficiente X1 :"))
-    x2 = float(input("Ingrese el coeficiente X2 :"))
-
-    print(f"El coeficiente A de su ecuación de la recta es: {a}")
-    print(f"El coeficiente B de su ecuación de la recta es: {b}")
-    print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
-    print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
-
-    print(f"Para la siguiente ecuacion:")
-    print(f"\t Y = {a}X + {b}")
+    if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+        print(f"El año {year} es bisiesto")
+    else:
+        print(f"El año {year} no es bisiesto")
 
 
-    print("Dado los siguiente puntos :")
-    print(f"\t P1 ({x1}, {(a * x1) + b})")
-    print(f"\t P2 ({x2}, {(a * x2) + b})")
-
-    y1 = (a * x1) + b
-    y2 = (a * x2) + b
-
-    distancia = ((x2 - x1)**2 + (y2-y1)**2) **0.5
-    print(f"La distancia entre ellos es: {distancia}")
